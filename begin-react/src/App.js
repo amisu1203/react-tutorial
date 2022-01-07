38de5e1c7d92e071806996257f262f1b0e946e4f
jisoo <doh_0112@naver.com>
2022-01-07 17:34:41 +0900

    실습 1

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+
+
+
+import React from 'react';
+import Hello from './hello';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕하세요?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Hello name="greeting"/>
+  )
 }
 
 export default App;
--- begin-react/src/hello.js
+++ begin-react/src/hello.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 // 컴포넌트 이름은 대문자로 시작
-function Hello() {
+function Hello(props) {
+    console.log(props);
     return <div>안녕하세요</div>;
 }
 
